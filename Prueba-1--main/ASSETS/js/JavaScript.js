6302d173478468ecc8d17c67c118bce9b086a704
pxshoo <106977053+pxshoo@users.noreply.github.com>
2024-04-11 22:08:42 -0400

    Add files via upload

--- Prueba-1--main/ASSETS/js/JavaScript.js
+++ Prueba-1--main/ASSETS/js/JavaScript.js
@@ -9,9 +9,11 @@ toggle.onclick = function(){
 document.querySelector('.toggle').addEventListener('click', function() {
     var container = document.getElementById('container');
     var toggle = document.querySelector('.toggle');
+
     
     if (toggle.style.left === '1px') {
         toggle.style.left = container.offsetWidth - toggle.offsetWidth - 1 + 'px';
+        
     } else {
         toggle.style.left = '1px';
     }
