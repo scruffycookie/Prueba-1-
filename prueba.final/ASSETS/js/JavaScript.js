6c48a4882062b8f7841923fa9a2ad7acbd3cb5c0
pxshoo <106977053+pxshoo@users.noreply.github.com>
2024-05-24 09:04:15 -0400

    Add files via upload

---
+++ prueba.final/ASSETS/js/JavaScript.js
@@ -0,0 +1,22 @@
+var toggle = document.getElementById('container');
+var body = document.querySelector('body');
+
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
+    juegos.classList.toggle('active');
+}
+
+document.querySelector('.toggle').addEventListener('click', function() {
+    var container = document.getElementById('container');
+    var toggle = document.querySelector('.toggle');
+
+    
+    if (toggle.style.left === '1px') {
+        toggle.style.left = container.offsetWidth - toggle.offsetWidth - 1 + 'px';
+        
+    } else {
+        toggle.style.left = '1px';
+    }
+});
+
